3459be97f0808effd9161a15281b562b0abf2b47
jakemanley <jbmanley45@gmail.com>
2023-06-09 10:28:24 -0700

    adding resume as pdf

--- src/app/Home.js
+++ src/app/Home.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 
 const Home = () => {
+    
     // about me
     // resume attachement
     // contact me
@@ -9,6 +10,17 @@ const Home = () => {
       <section>
         <h2>About Me</h2>
         <p>I am a web developer passionate about creating awesome websites.</p>    
+        <p>
+            I am a highly skilled developer with extensive expertise in a wide range of technologies, including HTML, database management, CSS, MySQL, Ruby on Rails, React.js, JavaScript, and Git. My commitment to continuous learning allows me to stay up-to-date with the latest industry trends and deliver exceptional results.
+            Drawing upon my diverse background in web development, sales, recruiting, communication, and leadership, I bring a unique and multifaceted perspective to every project I undertake. I thrive in collaborative environments, leveraging teamwork and effective communication as catalysts for success.
+            My passion for crafting exceptional digital experiences and my unwavering dedication to excellence drive me to consistently exceed expectations. I am excited to contribute my expertise and skills to create innovative solutions and deliver outstanding outcomes for your organization.
+        </p>
+    </section>
+    <br/>
+    <section>
+        <h2>Resume</h2>
+        {/* <a href={}></a> */}
+        <p>Resume</p>
     </section>
     </div>
   )
