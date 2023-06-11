2ee31d62e6474045eb395856b7c42f22b2b1444b
jakemanley <jbmanley45@gmail.com>
2023-06-10 21:26:10 -0700

    testing pdf in sep react app

--- src/app/Resume.js
+++ src/app/Resume.js
@@ -5,12 +5,15 @@ import React, {useState} from 'react';
 // import { Document, Page, pdfjs } from "react-pdf";
 // import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
 // pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
-// import { Document, Page, pdfjs } from "react-pdf";
+import { Document, Page, pdfjs } from "react-pdf";
 // import pdfjsWorker from "react-pdf/node_modules/pdfjs-dist/build/pdf.worker.entry";
-
 // pdfjs.GlobalWorkerOptions.workerSrc = pdfjsWorker;
-import { Worker, Viewer } from '@react-pdf-viewer/core';
+// import { Worker, Viewer } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
+
+pdfjs.GlobalWorkerOptions.workerPort = new Worker(
+  'node_modules/pdfjs-dist/build/pdf.worker.entry.js',
+);
 
 const Resume = () => {
     const [error, setError] = useState(null);
@@ -20,15 +23,16 @@ const Resume = () => {
       };
     console.log(error);
 
+
   return (
     
     <div>
     <div>
-      <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js" onLoadError={handleLoadError}>
-        <Viewer fileUrl="./resume.pdf">
+      <Document workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js" onLoadError={handleLoadError}>
+        <Page fileUrl="./resume.pdf">
 
-        </Viewer>
-      </Worker>
+        </Page>
+      </Document>
     </div>
     </div>
   )
