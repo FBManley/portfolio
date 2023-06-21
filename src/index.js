870d0ab262e8b6da7039767e9752eee16423a132
jakemanley <jbmanley45@gmail.com>
2023-06-21 11:58:49 -0700

    react 17 not supported, starting over new build to avoid refactor hellscape

--- src/index.js
+++ src/index.js
@@ -1,35 +1,23 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM  from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.css';
+import { createRoot } from 'react-dom/client';
+import {BrowserRouter } from 'react-router-dom';
+// import { composeWithDevTools } from '@redux-devtools/extension';
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+  domNode
 );
 
 reportWebVitals();
-// import React from 'react';
-// import ReactDOM  from 'react-dom/client';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-// import './index.css';
-// import { createRoot } from 'react-dom/client';
-// import {BrowserRouter } from 'react-router-dom';
-// // import { composeWithDevTools } from '@redux-devtools/extension';
-// const domNode = document.getElementById('root');
-// const root = createRoot(domNode);
-
-// // const root = ReactDOM.createRoot(document.getElementById('root'));
-
-// root.render(
-//   <React.StrictMode>
-//     <BrowserRouter>
-//     <App />
-//     </BrowserRouter>
-//   </React.StrictMode>
-// );
-
-// reportWebVitals();
